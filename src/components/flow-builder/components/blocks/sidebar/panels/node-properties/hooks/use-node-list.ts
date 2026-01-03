6c97d85006cdd28d1ca2ee6f7219c5380175a85b
williamfan <240200124@qq.com>
2026-01-03 21:03:52 +0800

    deploy

--- src/components/flow-builder/components/blocks/sidebar/panels/node-properties/hooks/use-node-list.ts
+++ src/components/flow-builder/components/blocks/sidebar/panels/node-properties/hooks/use-node-list.ts
@@ -1,11 +1,12 @@
 import { useMemo } from "react";
 import type { Node } from "@xyflow/react";
-import { BuilderNode } from "../../../../types";
+import { BuilderNode, BuilderNodeType } from "../../../../types";
 import { getNodeDetail } from "../../../../utils";
 
 export function useNodeList(nodes: Node[]) {
   return useMemo(() => {
     return nodes
+      .filter((n): n is Node & { type: BuilderNodeType } => !!n.type)
       .slice()
       .sort((a, b) => {
         if (a.type === BuilderNode.START) return -1;
